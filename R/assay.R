a91eb9757524f88c50c63ad69b2971a02302baf7
rintukutum <rintukutum@gmail.com>
2018-11-30 11:23:51 +0530

    updated

--- R/assay.R
+++ R/assay.R
@@ -1,3 +1,8 @@
+##' @name get.project.assay
+##' @title Get a Project information from PRIDE
+##' @description Get a project information based on pride ID from PRIDE database
+##' @author RINTU KUTUM
+##' @export
 get.project.assay <- function(accession = 'PXD000001'){
   baseURL <- 'https://www.ebi.ac.uk:443/pride/ws/archive/assay/list/project/'
   url <- paste0(
@@ -8,6 +13,12 @@ get.project.assay <- function(accession = 'PXD000001'){
   return(queryContent)
 }
 
+##' @name get.project.assay.count
+##' @title Get a Project information from PRIDE
+##' @description Get a project information based on pride ID from PRIDE database
+##' @author RINTU KUTUM
+##' @export
+
 get.project.assay.count <- function(accession = 'PXD000001'){
   baseURL <- 'https://www.ebi.ac.uk:443/pride/ws/archive/assay/count/project/'
   url <- paste0(
@@ -17,6 +28,12 @@ get.project.assay.count <- function(accession = 'PXD000001'){
   queryContent <- connectionStatus(resposeServer = httr::GET(url))
   return(queryContent)
 }
+
+##' @name get.assay
+##' @title Get a Project information from PRIDE
+##' @description Get a project information based on pride ID from PRIDE database
+##' @author RINTU KUTUM
+##' @export
 
 get.assay <- function(assay.accession = '22134'){
   baseURL <- 'https://www.ebi.ac.uk:443/pride/ws/archive/assay/'
